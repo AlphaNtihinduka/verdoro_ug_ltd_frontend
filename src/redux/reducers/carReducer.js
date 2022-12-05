4ae6ee82501144b28808b039e3a3d509789ee23a
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-12-05 15:31:59 +0300

    Add car id

--- src/redux/reducers/carReducer.js
+++ src/redux/reducers/carReducer.js
@@ -2,12 +2,16 @@ import { ActionTypes } from "../constants/actionTypes";
 
 const initialState = {
     cars: [
-        { name: "Genesis GV70",
+        { 
+        id:1,
+        name: "Genesis GV70",
         description: "Genesis GV70 models. The GV70 is a small luxury SUV available in two trim levels: 2.5T and 3.5T Sport",
         price_per_day: 800,
         image: "https://images.cars.com/cldstatic/wp-content/uploads/genesis-gv70-2022-03-exterior-grey-profile-suv-scaled.jpg"
         },
-        { name: "Ram 1500 Limited Longhorn",
+        { 
+        id:2,    
+        name: "Ram 1500 Limited Longhorn",
         description: "The 2022 RAM 1500 Longhorn is offered with a 3.6-liter V6 gas engine. It has an eTorque system, which gives it mild-hybrid capabilities. ",
         price_per_day: 1000,
         image: "https://images.cars.com/cldstatic/wp-content/uploads/ram-1500-limited-2022-exterior-oem-profile-scaled.jpg"
